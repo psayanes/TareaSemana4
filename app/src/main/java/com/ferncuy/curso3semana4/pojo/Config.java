5e6f282022a9914b30557e70bc1cbe0cfafd7623
jsayanes <psayanes@gmail.com>
2020-10-20 22:27:11 -0300

    primer commit

---
+++ app/src/main/java/com/ferncuy/curso3semana4/pojo/Config.java
@@ -0,0 +1,13 @@
+/*
+ * Copyright (c) 2020
+ * By Pablo Fernandez
+ * ferning98@gmail.com
+ */
+
+package com.ferncuy.curso3semana4.pojo;
+
+
+public class Config {
+    public static final String EMAIL ="psayanes@gmail.com";
+    public static final String PASSWORD="pepe1234";
+}
